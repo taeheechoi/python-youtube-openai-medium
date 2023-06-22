b257617dfd62c7f3216c537236ff79154b373c32
Tae Hee Choi <dadac76@hotmail.com>
2023-06-21 19:47:53 -0700

    3rd try to build test

--- app/database.py
+++ app/database.py
@@ -1,13 +1,15 @@
-from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 from .config import get_config
 
 config = get_config()
 DATABASE_URL = config.DATABASE_URL
 
-engine = create_engine(DATABASE_URL, connect_args={
-    "check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+    
